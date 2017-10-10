98863179f452555434c91063a2f1499d05bc8b66
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2017-10-09 22:20:36 -0400

    HW07: Add comments

--- hw07/src/InvertImage.java
+++ hw07/src/InvertImage.java
@@ -1,3 +1,11 @@
+/*
+ * CSCI 392
+ * HW07 - Histogram Class
+ * Peter Nguyen
+ * Due: Oct. 10, 2017
+ */
+
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -12,6 +20,7 @@ public class InvertImage {
             return;
         }
 
+        // Check if file extension is ".jpg"
         if (!args[0].endsWith(".jpg")) {
             System.out.println("Error: File must be a JPEG");
         }
@@ -20,6 +29,7 @@ public class InvertImage {
         File input = new File(filePath.toString());
         BufferedImage image;
 
+        // Try to read image
         try {
             image = ImageIO.read(input);
         } catch (Exception e) {
@@ -27,20 +37,26 @@ public class InvertImage {
             return;
         }
 
+        // For each pixel in image
         for (int i = 0, width = image.getWidth(); i < width; ++i) {
             for (int j = 0, height = image.getHeight(); j < height; ++j) {
-                Color inverted = new Color(image.getRGB(i, j));
+                Color inverted = new Color(image.getRGB(i, j)); // Get color of pixel
+
+                // Invert color
                 inverted = new Color(
                         255 - inverted.getRed(),
                         255 - inverted.getGreen(),
                         255 - inverted.getBlue()
                 );
 
-                image.setRGB(i, j, inverted.getRGB());
+                image.setRGB(i, j, inverted.getRGB()); // Set pixel to inverted color
             }
         }
 
+        // Prefix "inv_" to filename
         String outputPath = filePath.getParent() + File.separator + "inv_" + filePath.getFileName();
+
+        // Try to save file
         try {
             System.out.println(outputPath);
             File output = new File(outputPath);
